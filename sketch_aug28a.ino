aef99983c1dbe315558010da3e80d2dba4a5b9ac
0123456789rajesh <95772573+0123456789rajesh@users.noreply.github.com>
2024-12-03 17:30:24 +0530

    Update sketch_aug28a.ino

--- sketch_aug28a.ino
+++ sketch_aug28a.ino
@@ -11,11 +11,11 @@ void setup() {
 }
 
 void loop() {
-  // // Turn the RGB strip Red for 2 seconds
-  // analogWrite(redPin, 255);    // Max brightness for Red
-  // analogWrite(greenPin, 0);    // Green off
-  // analogWrite(bluePin, 0);     // Blue off
-  // delay(10000);  // Wait for 2 seconds
+  // Turn the RGB strip Red for 2 seconds
+  analogWrite(redPin, 255);    // Max brightness for Red
+  analogWrite(greenPin, 0);    // Green off
+  analogWrite(bluePin, 0);     // Blue off
+  delay(10000);  // Wait for 2 seconds
   
   // Turn the RGB strip Green for 2 seconds
   analogWrite(redPin, 0);
@@ -23,47 +23,47 @@ void loop() {
   analogWrite(bluePin, 0);
   delay(10000);  // Wait for 2 seconds
 
-  //    // Turn the RGB strip yellow for 2 seconds
-  // analogWrite(redPin, 255);
-  // analogWrite(greenPin, 100);
-  // analogWrite(bluePin, 0);   // Max brightness for Blue
-  // delay(10000);  // Wait for 2 seconds
+     // Turn the RGB strip yellow for 2 seconds
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 100);
+  analogWrite(bluePin, 0);   // Max brightness for Blue
+  delay(10000);  // Wait for 2 seconds
   
-  // // Turn the RGB strip Blue for 2 seconds
-  // analogWrite(redPin, 0);
-  // analogWrite(greenPin, 0);
-  // analogWrite(bluePin, 255);   // Max brightness for Blue
-  // delay(10000);  // Wait for 2 seconds
+  // Turn the RGB strip Blue for 2 seconds
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 255);   // Max brightness for Blue
+  delay(10000);  // Wait for 2 seconds
 
-  //    // Turn the RGB strip yellow for 2 seconds
-  // analogWrite(redPin, 255);
-  // analogWrite(greenPin, 100);
-  // analogWrite(bluePin, 0);   // Max brightness for Blue
-  // delay(3000);  // Wait for 2 seconds
+     // Turn the RGB strip yellow for 2 seconds
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 100);
+  analogWrite(bluePin, 0);   // Max brightness for Blue
+  delay(3000);  // Wait for 2 seconds
 
     
-  // // Turn the RGB strip Blue for 2 seconds
-  // analogWrite(redPin, 0);
-  // analogWrite(greenPin, 0);
-  // analogWrite(bluePin, 255);   // Max brightness for Blue
-  // delay(1500);  // Wait for 2 seconds
+  // Turn the RGB strip Blue for 2 seconds
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 255);   // Max brightness for Blue
+  delay(1500);  // Wait for 2 seconds
 
-  //   // Turn the RGB strip PINK for 2 seconds
-  // analogWrite(redPin, 255);
-  // analogWrite(greenPin, 0);
-  // analogWrite(bluePin, 100);   // Max brightness for Blue
-  // delay(10000);  // Wait for 2 seconds
+    // Turn the RGB strip PINK for 2 seconds
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 100);   // Max brightness for Blue
+  delay(10000);  // Wait for 2 seconds
 
-  //   // Turn the RGB strip yellow for 2 seconds
-  // analogWrite(redPin, 255);
-  // analogWrite(greenPin, 100);
-  // analogWrite(bluePin, 0);   // Max brightness for Blue
-  // delay(3000);  // Wait for 2 seconds
+    // Turn the RGB strip yellow for 2 seconds
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 100);
+  analogWrite(bluePin, 0);   // Max brightness for Blue
+  delay(3000);  // Wait for 2 seconds
   
 
-  // // Turn the RGB strip OFF for 2 seconds
-  // analogWrite(redPin, 0);
-  // analogWrite(greenPin, 0);
-  // analogWrite(bluePin, 0);
-  // delay(2000);  // Wait for 2 seconds
+  // Turn the RGB strip OFF for 2 seconds
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 0);
+  delay(2000);  // Wait for 2 seconds
 }
